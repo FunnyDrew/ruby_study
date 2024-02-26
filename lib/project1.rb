9c5b74d9d89311d1036f4ecb80d3d6797fa74364
FunnyDrew <rybin.ag@gmail.com>
2024-02-26 12:06:56 +0300

    state 26.02/12:08

--- lib/project1.rb
+++ lib/project1.rb
@@ -8,10 +8,36 @@ module HexletCode
 			"<#{init_arr.join(" ")}>"
 		end
 	end
+
+	class FormBuilder
+		attr_accessor :user
+
+		def initialize(user)
+			@user = user
+		end
+
+		def input(option={})
+			HexletCode::Tag.build("input")
+		end
+	end
+
+	def self.form_for(user)
+
+		form = FormBuilder.new(user)
+		yield form
+
+		"<form>"
+	end
+
+
+
+
 end
 
 User = Struct.new(:name, :job)
 user = User.new(name: "John", job: "home" )
 
 p HexletCode::Tag.build('input',{:type => "text", :value => "name"})
-p user
+p HexletCode.form_for user do |f|
+	f.input
+end
